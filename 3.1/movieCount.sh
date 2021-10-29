2a921c4a6c8d5cb6b37c3b5f8be8865c39b3156b
NourSleiman <32310672+NourSleiman@users.noreply.github.com>
2021-10-29 16:55:26 -0400

    Add files via upload

--- 3.1/movieCount.sh
+++ 3.1/movieCount.sh
@@ -8,15 +8,20 @@ declare -a genres=("Action" "Adventure" "Animation"
 
 for i in {1915..2018}
 do
+	# For each year, the line found is written to totalMovies.txt to get the total count of movies for that year
+	# The Year and the count is appended to movieDistribution.txt
 	grep "($i)" $INPUT > ./totalMovies.txt
 	totalCount=`awk 'END { print NR }' ./totalMovies.txt`
 	echo "$i - Total Movies: $totalCount" >> ./movieDistribution.txt
 
+	# For each year, each genre is searched for and written to genreCount.txt to get the total count per genre
+	# The genre and the count is appended to movieDistribution.txt
 	for g in "${genres[@]}"
 	do
 		awk "/$g/ {print}" totalMovies.txt > ./genreCount.txt
 		count=`awk 'END { print NR }' ./genreCount.txt`
 		echo "$g : $count" >> ./movieDistribution.txt
 	done
+	# Line break to separate between the years
 	echo "----------------" >> ./movieDistribution.txt
 done
